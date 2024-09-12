391682871780f605d5698855db056b7ba5a4d219
Andrei Mihai <andreimihai2305is@gmail.com>
2024-09-12 19:43:15 +0300

    translated more functions from python to c

--- main.c
+++ main.c
@@ -42,18 +42,57 @@ void draw_grid(Grid grid)
     DrawLineV((Vector2){.x = 0, .y = 1}, (Vector2){.x = WIDTH, .y = 1}, RAYWHITE);
     DrawLineV((Vector2){.x = 1, .y = 0}, (Vector2){.x = 1, .y = HEIGHT}, RAYWHITE);
     DrawLineV((Vector2){.x = WIDTH, .y = 0}, (Vector2){.x = WIDTH, .y = HEIGHT}, RAYWHITE);
-    for (int i = 1; i <= GRID_SIZE; i++) {
+    DrawLineV((Vector2){.x = 0, .y = HEIGHT}, (Vector2){.x = WIDTH, .y = HEIGHT}, RAYWHITE);
+
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++){
+            if (grid_at(grid, i, j) != 0) {
+                DrawRectangleV((Vector2){i*cell_size, j*cell_size}, (Vector2){cell_size, cell_size}, RAYWHITE);
+            }
+        }
+
         DrawLineV((Vector2){.x = i*cell_size, .y = 0}, (Vector2){.x = i*cell_size, .y = HEIGHT},  RAYWHITE);
         DrawLineV((Vector2){.x = 0, .y = i*cell_size}, (Vector2){.x = WIDTH, .y = i*cell_size}, RAYWHITE);
     }
 
 }
 
+Vector2 world_to_grid(Vector2 point)
+{
+    return (Vector2){ .x = point.x / WIDTH * GRID_SIZE, .y = point.y / HEIGHT * GRID_SIZE };
+}
+
+Vector2 grid_to_world(Vector2 point)
+{
+    return (Vector2){ .x = point.x * WIDTH / GRID_SIZE, .y = point.y * HEIGHT / GRID_SIZE };
+}
+
+void draw_line(Vector2 p1, Vector2 p2)
+{
+    Vector2 p1w = grid_to_world(p1);
+    Vector2 p2w = grid_to_world(p2);
+    DrawLineV(p1w, p2w, RED);
+}
+
+Vector2 get_line_eq(Vector2 p1, Vector2 p2)
+{
+    float m = 0.0;
+    if (p1.x != p2.x) m = (p2.y - p1.y) / (p2.x - p1.x);
+    
+    float n = p2.y - m * p2.x;
+
+    return (Vector2){.x = m, .y = n};
+}
+
+
+
+
 int main(void)
 {
     Grid g = {0};
     make_grid(&g, GRID_SIZE, GRID_SIZE); 
-    grid_at(g, 2, 2) = true;
+    grid_at(g, 2, 2) = 1;
+
     for (int i = 0; i < g.rows; i++){ 
         for (int j = 0; j < g.cols; j++) {
             printf("%d ", grid_at(g, i, j));
@@ -69,6 +108,7 @@ int main(void)
         BeginDrawing();
             ClearBackground(BLACK);
             draw_grid(g);
+            draw_line((Vector2){1,1}, (Vector2){2,4});
 
         EndDrawing();
     }
