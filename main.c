c5b2228e6f03cf0f4e4001a93f9a0bcfc678d10b
Andrei Mihai <andreimihai2305is@gmail.com>
2024-09-13 11:17:32 +0300

    continuing code translation to c

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 #include "raylib/raylib.h"
-
+#include "raylib/raymath.h"
+#include "math.h"
 
 #define GRID_SIZE  8
 #define FPS        60
 #define WIDTH      800
 #define HEIGHT     800
-#define RADIUS     10
+#define RADIUS     10.0f
 #define EPS        1e-4
 
 
@@ -57,6 +58,7 @@ void draw_grid(Grid grid)
 
 }
 
+// TODO: Stop using global constants
 Vector2 world_to_grid(Vector2 point)
 {
     return (Vector2){ .x = point.x / WIDTH * GRID_SIZE, .y = point.y / HEIGHT * GRID_SIZE };
@@ -74,6 +76,12 @@ void draw_line(Vector2 p1, Vector2 p2)
     DrawLineV(p1w, p2w, RED);
 }
 
+void draw_point(Vector2 p)
+{
+    Vector2 pw = grid_to_world(p);
+    DrawCircleV(pw, RADIUS, RED);
+}
+
 Vector2 get_line_eq(Vector2 p1, Vector2 p2)
 {
     float m = 0.0;
@@ -84,7 +92,35 @@ Vector2 get_line_eq(Vector2 p1, Vector2 p2)
     return (Vector2){.x = m, .y = n};
 }
 
+// TODO: Refactor the way collisions are computed
+Vector2 cast_ray(Vector2 p1, Vector2 p2)
+{
+    Vector2 line_eq = get_line_eq(p1, p2);
+    Vector2 diff = Vector2Subtract(p2, p1);
+    float m = line_eq.x;
+    float n = line_eq.y;
+    
+    Vector2 dx = {0};
+    Vector2 dy = {0};
 
+    // Find closest X axis collision
+    if (m) {
+        if (diff.y > 0) dx = (Vector2){.x = (ceil(p2.y) - n) / m, .y = ceil(p2.y)};
+        else dx = (Vector2){.x = (floorf(p2.y) - n) / m, .y = floorf(p2.y)};
+    } else dx = (Vector2){.x = 0, .y = n};
+
+    // Find Y axis collision
+    if (diff.x > 0) dy = (Vector2){.x = ceil(p2.x), .y = m * ceil(p2.x) + n};
+    else if (diff.x < 0) dy = (Vector2){.x = floorf(p2.x), .y = m * floorf(p2.x) + n};
+    else {
+        if (diff.y > 0) dy = (Vector2){.x = p2.x, .y = ceil(p2.y)};
+        else dy = (Vector2){.x = p2.x, .y = floorf(p2.x)};
+    }
+    
+    if (Vector2DistanceSqr(p2, dx) < Vector2DistanceSqr(p2, dy)) return dx;
+    return dy;
+
+}
 
 
 int main(void)
@@ -103,12 +139,16 @@ int main(void)
 
     InitWindow(WIDTH, HEIGHT, "Hello Raylib");
     SetTargetFPS(FPS); 
-        while(!WindowShouldClose()) 
+    while(!WindowShouldClose()) 
     {
+        Vector2 mouse_pos = world_to_grid(GetMousePosition());
+        Vector2 p = {1,1};
         BeginDrawing();
             ClearBackground(BLACK);
             draw_grid(g);
-            draw_line((Vector2){1,1}, (Vector2){2,4});
+            draw_line(p, mouse_pos);
+            draw_point(p);
+            draw_point(mouse_pos);
 
         EndDrawing();
     }
